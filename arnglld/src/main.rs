9da4855532d3b2ec408dec94743210003af8fa9f
Robert Quattlebaum <darco@deepdarc.com>
2022-02-18 11:52:51 -0800

    arnglld: Added audio device selection options.
    Also provided getters for the frame sender and frame receiver.

--- arnglld/src/main.rs
+++ arnglld/src/main.rs
@@ -19,8 +19,15 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+use anyhow::format_err;
 use clap::Parser;
+use cpal::traits::{DeviceTrait, HostTrait};
+use futures::executor::{block_on, block_on_stream};
+use futures::prelude::*;
+use log::info;
 use hamaddr::HamAddr;
+use quick_dsp::bell202::{Bell202Receiver, Bell202Sender};
+use quick_dsp::filter::IteratorExt as _;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -33,19 +40,152 @@ struct Opt {
     #[clap(short, long, parse(from_occurrences))]
     verbose: usize,
 
+    /// List audio devices
+    #[clap(long)]
+    list_devices: bool,
+
     #[clap(short, long)]
-    callsign: HamAddr,
+    callsign: Option<HamAddr>,
+
+    #[clap(long)]
+    input_audio_device: Option<String>,
+
+    #[clap(long)]
+    output_audio_device: Option<String>,
+}
+
+fn find_device<I: IntoIterator<Item = cpal::Device>>(
+    devices: I,
+    name: &str,
+) -> Option<cpal::Device> {
+    let lc_name = name.to_lowercase();
+    let mut devs = devices
+        .into_iter()
+        .filter_map(|d| Some((d.name().ok()?.to_lowercase(), d)))
+        .collect::<Vec<_>>();
+
+    // Try exact
+    if let Some((i, _)) = devs.iter().enumerate().find(|(_, (n, _))| n == &lc_name) {
+        return Some(devs.remove(i).1);
+    }
+
+    // Try prefix
+    if let Some((i, _)) = devs
+        .iter()
+        .enumerate()
+        .find(|(_, (n, _))| n.starts_with(&lc_name))
+    {
+        return Some(devs.remove(i).1);
+    }
+
+    // Any substring match
+    if let Some((i, _)) = devs
+        .iter()
+        .enumerate()
+        .find(|(_, (n, _))| n.contains(&lc_name))
+    {
+        return Some(devs.remove(i).1);
+    }
+
+    None
+}
+
+impl Opt {
+    fn get_output_device(&self) -> Result<cpal::Device, anyhow::Error> {
+        let host = cpal::default_host();
+
+        if let Some(name) = self.output_audio_device.as_ref() {
+            return find_device(host.output_devices()?, name.as_str())
+                .ok_or_else(|| format_err!("Cannot find output device matching {:?}", name));
+        }
+
+        host.default_output_device()
+            .ok_or_else(|| format_err!("no default output device"))
+    }
+
+    #[allow(dead_code)]
+    fn get_input_device(&self) -> Result<cpal::Device, anyhow::Error> {
+        let host = cpal::default_host();
+
+        if let Some(name) = self.input_audio_device.as_ref() {
+            return find_device(host.input_devices()?, name.as_str())
+                .ok_or_else(|| format_err!("Cannot find input device matching {:?}", name));
+        }
+
+        host.default_input_device()
+            .ok_or_else(|| format_err!("no default input device"))
+    }
+
+    #[allow(dead_code)]
+    fn get_packet_stream(&self) -> Result<Bell202Receiver, anyhow::Error> {
+        let device = self.get_input_device()?;
+        info!("Using input device {:?}", device.name());
+        let receiver = Bell202Receiver::new(&device)?;
+
+        Ok(receiver)
+    }
+
+    fn get_packet_sink(&self) -> Result<Bell202Sender, anyhow::Error> {
+        let device = self.get_output_device()?;
+        info!("Using output device {:?}", device.name());
+        let sender = Bell202Sender::new(&device)?;
+
+        Ok(sender)
+    }
 }
 
 fn main() {
     let opt = Opt::parse();
 
-    println!("Callsign: {}", opt.callsign);
-    println!("opt = {:?}", opt);
+    if opt.list_devices {
+        let host = cpal::default_host();
+        println!("Input Devices:");
+        for device in host.input_devices().expect("Unable to list input devices") {
+            if let Some(name) = device.name().ok() {
+                println!("\t{}", name);
+            }
+        }
+        println!("Output Devices:");
+        for device in host
+            .output_devices()
+            .expect("Unable to list output devices")
+        {
+            if let Some(name) = device.name().ok() {
+                println!("\t{}", name);
+            }
+        }
+        return;
+    }
 
     stderrlog::new()
         .quiet(opt.quiet)
         .verbosity(opt.verbose)
         .init()
         .unwrap();
+
+    println!("Callsign: {}", opt.callsign.expect("Missing callsign"));
+    println!("opt = {:?}", opt);
+
+    const X25: crc::Crc<u16> = crc::Crc::<u16>::new(&crc::CRC_16_IBM_SDLC);
+
+    let frame_string = format!(
+        "\x01\x00de {}: TEST: This is a test frame of ASCII text.",
+        opt.callsign.unwrap()
+    );
+    let frame = frame_string.bytes().append_crc(&X25);
+
+    let mut packet_sink = opt.get_packet_sink().unwrap();
+
+    println!("Sending test frame...");
+
+    // Play the test packet.
+    block_on(packet_sink.send(frame.collect())).unwrap();
+
+    println!("Listening for packets...");
+
+    let packet_stream = opt.get_packet_stream().unwrap();
+
+    for frame in block_on_stream(packet_stream) {
+        info!("Received: {:?}", hex::encode(frame));
+    }
 }
